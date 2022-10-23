5e21c4829806e3deca99e19efde1b34ca6422755
goal-ddol <sokky302@gmail.com>
2022-10-24 01:43:51 +0900

    Sample

---
+++ sample/00.sample.py
@@ -0,0 +1,22 @@
+from ast import While
+import random
+
+random_number = random.randint(1,100)
+
+#print(random_number)
+
+game_count = 1
+
+while True:
+            
+        my_number = int(input("1-100 사이의 숫자를 입력하세요"))
+        
+        if my_number > random_number:
+           print('Down!!')
+        elif my_number < random_number:
+           print('Up!!')
+        else:
+           print(f'축하합니다. {game_count}번 만에 맞추셨습니다.')
+           break
+        
+        game_count = game_count + 1
